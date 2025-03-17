100993998924d73b4a1f50dfc94431a6054dbfb0
Darwin Raudales <raudalesdarwin@gmail.com>
2025-03-17 16:07:38 -0600

    pruebas

--- server/app.js
+++ server/app.js
@@ -1,21 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const authRoutes = require('./routes/authUser');
-const authMiddleware = require('./middleware/authMiddleware');
+const express=require('express');
+const app =express();
 require('dotenv').config();
 
-const app = express();
+const PORT = process.env.PORT;
 
-// Middleware
-app.use(cors());
+
+
 app.use(express.json());
 
-// Rutas públicas
-app.use('/api/auth', authRoutes);
 
 
-// Iniciar servidor
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
