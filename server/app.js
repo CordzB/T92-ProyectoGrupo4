337856bd18facebac0577eb69e3fe040774a5057
Darwin Raudales <raudalesdarwin@gmail.com>
2025-03-18 15:27:16 -0600

    Autentication

--- server/app.js
+++ server/app.js
@@ -1,15 +1,34 @@
 const express=require('express');
 const app =express();
+const axios = require('axios');
+const cors = require('cors');
+
 require('dotenv').config();
+
 
 const PORT = process.env.PORT;
 
+// Middlewares
+app.use(cors()); // Permite peticiones desde otros dominios
+app.use(express.json()); // Para manejar JSON en las peticiones
 
 
-app.use(express.json());
+// Rutas
+app.use('/auth', authRoutes); // Rutas de autenticación (login, registro)
+app.use('/usuarios', userRoutes); // Rutas relacionadas con usuarios
 
+// Ruta de prueba
+app.get('/', (req, res) => {
+    res.send('API del Autolote');
+});
 
+// Manejo de errores global
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ status: 500, message: 'Error del servidor' });
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+// Servidor en marcha
+app.listen(PORT, () => {
+    console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
 });
--- server/routes/authUsers.js
+++ server/routes/authUsers.js
@@ -7,5 +7,52 @@ const bcrypt = require('bcrypt');
 
 require('dotenv').config();
 
+router.post('/login', async (req, res) => {
+    const { correo, contraseña } = req.body;
+
+    const sql = 'SELECT * FROM usuarios WHERE correo = ?';
+
+    pool.query(sql, [correo], async (err, resultado) => {
+        if (err) {
+            return res.status(500).json({ status: 500, message: 'Error del servidor' });
+        }
+
+        if (resultado.length === 0) {
+            return res.status(401).json({ status: 401, message: 'Credenciales inválidas...' });
+        }
+
+        let user = resultado[0];
+
+        const isMatch = await bcrypt.compare(contraseña, user.contraseña);
+
+        if (!isMatch) {
+            return res.status(401).json({ status: 401, message: 'Credenciales inválidas...' });
+        }
+
+        const token = jwt.sign(
+            { id: user.id_usuario, correo: user.correo },
+            process.env.SECRET_KEY,
+            { expiresIn: '1h' }
+        );
+        res.json({ status: 200, message: 'Success', token });
+    });
+});
+
+router.post('/usuarios', authMiddleware, async (req, res) => {
+    const { nombre, correo, contraseña, rol } = req.body;
+
+    const sql = 'INSERT INTO usuarios (nombre, correo, contraseña, rol) VALUES (?, ?, ?, ?)';
+
+    const saltRound = 10;
+    const passwordEncrypt = await bcrypt.hash(contraseña, saltRound);
+
+    pool.query(sql, [nombre, correo, passwordEncrypt, rol], (err, resultado) => {
+        if (err) {
+            return res.status(500).json({ status: 500, message: 'Error del servidor' });
+        }
+
+        res.json({ status: 200, message: 'Success', id_usuario: resultado.insertId });
+    });
+});
 
 module.exports = router;
