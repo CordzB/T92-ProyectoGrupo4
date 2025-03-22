0d950dd6f32a48bf9ba4843223f8e3b86a4fcbe6
Darwin Raudales <raudalesdarwin@gmail.com>
2025-03-22 14:33:17 -0600

    logic actualizado

--- client/src/app/component/login/login.component.ts
+++ client/src/app/component/login/login.component.ts
@@ -1,32 +1,32 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../../services/auth.service'; // ng generate service services/auth
+import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  username: string = '';
+  nombre: string = '';
   contrasenia: string = '';
   errorMessage: string = '';
 
-  constructor(private authService: AuthService,
-    private router: Router
-  ) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   login() {
     const credentials = {
-      username: this.username,
-      password: this.contrasenia
+      nombre: this.nombre,
+      contrasena: this.contrasenia
     };
+    
     this.errorMessage = '';
+    
     this.authService.login(credentials).subscribe({
-      
       next: (response) => {
         console.log('Login exitoso:', response);
         localStorage.setItem('token', response.token);
@@ -42,7 +42,6 @@ export class LoginComponent {
           this.errorMessage = 'Ocurrió un error inesperado. Intenta de nuevo.';
         }
       }
-
     });
   }
 }
--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -2,16 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000/api'; // Cambia esto según tu backend
+  private apiUrl = 'http://localhost:3000/api'; // Ajusta esto según tu backend
 
   constructor(private http: HttpClient) {}
 
-  login(userData: { username: string; password: string }): Observable<any> {
+  login(userData: { nombre: string; contrasena: string }): Observable<any> {
     return this.http.post(`${this.apiUrl}/login`, userData);
   }
 }
