4fe38b9795cdd68309e6cbec45958739cd7edc7f
Darwin Raudales <raudalesdarwin@gmail.com>
2025-03-22 12:37:15 -0600

    Muestra ingeniero
    Este es el codigo que nos dio sin modificar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'client';
+  
+  constructor(private router: Router) {}
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token'); // Verifica si hay un token
+  }
+
+  logout() {
+    localStorage.removeItem('token'); // Elimina el token
+    this.router.navigate(['/login']); // Redirige al login
+  }
 }
